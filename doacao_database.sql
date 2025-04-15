cccea8d0025c6d779c74da3b77ca98cea2ccab7a
Evandro-Nicolau <69369795+Evnz25@users.noreply.github.com>
2025-04-15 09:25:54 -0300

    Update doacao_database.sql

--- doacao_database.sql
+++ doacao_database.sql
@@ -14,3 +14,8 @@ insert into animals(id_animal, age, gender, image_path, name, race, specie) valu
 (1, 2, "Macho", "/uploads/ZecaPic.png", "Zeca", "Vira Lata", "Cachorro"),
 (2, 1, "Fêmea", "/uploads/NinaPic.png", "Nina", "Vira Lata", "Cachorro"),
 (3, 4, "Fêmea", "/uploads/BellaPic.png", "Nina", "Vira Lata", "Cachorro");
+
+insert into medical_conditions(id, injuries, sickness, medicine, vaccine, castrated) values
+(1, "Não tem", "Não tem", "Não tem", "Não tem", "Sim"),
+(2, "Não tem", "Não tem", "Não tem", "Não tem", "Sim"),
+(3, "Não tem", "Não tem", "Não tem", "Não tem", "Sim");
